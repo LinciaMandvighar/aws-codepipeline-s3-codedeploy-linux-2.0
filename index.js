dc014cdd28a444b8e41e3b2b51672d3233c24a4d
LinciaMandvighar <linjookmandvighar95@student.sfit.ac.in>
2024-11-10 21:26:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+puthon
 import json
 def lambda_handler(event, context):
     student_name = "Lincia Mandvighar"
@@ -6,3 +7,36 @@ def lambda_handler(event, context):
         'statusCode': 200,
         'body': message
     }
+
+nodejs
+export const handler = async (event) => {
+  // TODO implement
+  const name = "Lincia Mandvighar"
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify(Hello ${name}),
+  };
+  return response;
+};
+
+//lambda s3 bucketq9
+import json
+
+
+def lambda_handler(event, context):
+    # Extract bucket name and object key from the event
+    for record in event['Records']:
+        bucket_name = record['s3']['bucket']['name']
+        object_key = record['s3']['object']['key']
+       
+        # Log the message
+        print(f"An object has been added to the bucket {bucket_name}: {object_key}")
+
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Log completed')
+    }
+
+
+
